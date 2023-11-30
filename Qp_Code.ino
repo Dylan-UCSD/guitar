faf009f847b58e821d981d5d48fccb5a04f180f9
Dylan-UCSD <143430775+Dylan-UCSD@users.noreply.github.com>
2023-11-30 14:49:32 -0800

    Add files via upload

--- Qp_Code.ino
+++ Qp_Code.ino
@@ -1,11 +1,16 @@
 #include <Servo.h>
 #define MAX_ANGLE 60
+#define FRET_ANGLE 100
 
+int interrupt[6] = {2,3,18,19,20,21};
+int buttons[4] = {22,24,26,28};
+int buttonsState[4] = {0,0,0,0};
 Servo servoA,servoB,servoC,servoD,servoE,servoF;
+Servo fret1,fret2,fret3,fret4,fret5,fret6,fret7,fret8;
 Servo servoArray[6] = {servoA,servoB,servoC,servoD,servoE,servoF};
-//Servo servoNotes[15] = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o};
+Servo servoNotes[8] = {fret1,fret2,fret3,fret4,fret5,fret6,fret7,fret8};
 int servoState[6] = {0,0,0,0,0,0};
-const int signalin = 12;
+const int signalin = 9;
 int interruptNumber = 0;
 unsigned long debounce = 100;     // Debounce delay for cleaner interrupt input
 unsigned long buttonStartTime = 0;// Record button start time
@@ -51,36 +56,103 @@ void moveAnyServos(Servo* servos,int* setStates){
   }
  
 }
+void interrupterA(){
+   if (millis() - buttonStartTime < debounce) { // Debounce
+    return;
+  }
+  buttonStartTime = millis();
+  Serial.print("Moving A servo");
+  sendServo(&servoA);
+}
+void interrupterB(){
+  if (millis() - buttonStartTime < debounce) { // Debounce
+    return;
+  }
+  buttonStartTime = millis();
+  Serial.print("Moving B servo");
+  sendServo(&servoB);
+}
+void interrupterC(){
+   if (millis() - buttonStartTime < debounce) { // Debounce
+    return;
+  }
+  buttonStartTime = millis();
+  Serial.print("Moving C servo");
+  sendServo(&servoC);
+}
+void interrupterD(){
+   if (millis() - buttonStartTime < debounce) { // Debounce
+    return;
+  }
+  buttonStartTime = millis();
+  Serial.print("Moving D servo");
+  sendServo(&servoD);
+}
+void interrupterE(){
+  if (millis() - buttonStartTime < debounce) { // Debounce
+    return;
+  }
+  buttonStartTime = millis();
+  Serial.print("Moving E servo");
+  sendServo(&servoE);
+}
+void interrupterF(){
+  if (millis() - buttonStartTime < debounce) { // Debounce
+    return;
+  }
+  buttonStartTime = millis();
+  Serial.print("Moving F servo");
+  sendServo(&servoF);
+}
+void sendFret(Servo *serv){
+  if(serv->read() == 0){
+      serv->write(FRET_ANGLE);
+   }else{
+      serv->write(0);
+   }
+}
 void setup() {
   Serial.begin(9600);
   // put your setup code here, to run once:
  Serial.println(sizeof(servoArray)/sizeof(servoArray[0]));
- for(int i = 0; i <= sizeof(servoArray)/sizeof(servoArray[0]);i++){
+ for(int i = 1; i <= sizeof(servoArray)/sizeof(servoArray[0]);i++){
     servoArray[i].attach(i+3);
     servoArray[i].write(0);
     Serial.println(i);
   }
-  pinMode(signalin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(signalin), interruptdetected, FALLING);
-  /*for(int i = 0; i <= sizeof(servoArray)/sizeof(servoArray[0]);i++){
-    servoArray[i].attach(i+3);
+  /*pinMode(signalin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(signalin), interruptdetected, FALLING);*/
+  for(int i = 0; i < (sizeof(servoNotes)/sizeof(servoNotes[0]));i+=2){
+    servoArray[i].attach(i+10);
+    servoArray[i+1].attach(i+10);
     servoArray[i].write(0);
     Serial.println(i);
-  }*/ 
+  } 
   //TODO initialize all of the servos for the notes.
   int arr[6] = {0,0,1,0,0,1};
   moveAnyServos(servoArray,arr);
+
+  for(int i = 0; i < 6; i++){
+       pinMode(interrupt[i],INPUT_PULLUP);
+   }
+  attachInterrupt(digitalPinToInterrupt(interrupt[0]),interrupterA,RISING);
+  attachInterrupt(digitalPinToInterrupt(interrupt[1]),interrupterB,RISING);
+  attachInterrupt(digitalPinToInterrupt(interrupt[2]),interrupterC,RISING);
+  attachInterrupt(digitalPinToInterrupt(interrupt[3]),interrupterD,RISING);
+  attachInterrupt(digitalPinToInterrupt(interrupt[4]),interrupterE,RISING);
+  attachInterrupt(digitalPinToInterrupt(interrupt[5]),interrupterF,RISING);
+  for(int i = 0; i < 4;i++){
+    pinMode(buttons[i],INPUT);
+   }
 }
-int buttonState = 0;
+
+//int buttonState = 0;
 void loop() {
-  // put your main code here, to run repeatedly:
-  //sendServo(&servoF);
-  //delay(500);
-  buttonState = digitalRead(signalin);
-  if(buttonState == HIGH){
-    if (millis() - buttonStartTime >= debounce) { // Debounce
-      sendServo(&servoF);
+  for(int i = 0; i < 4;i++){
+    buttonsState[i] = digitalRead(buttons[i]);
+    if(buttonsState[i] == 1){
+      sendFret(servoNotes[i*2]);
     }
-    buttonStartTime = millis();
   }
+   
 }
